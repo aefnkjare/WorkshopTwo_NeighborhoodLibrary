a0e656d9f6fff9b2540f382cdd4006f7e8a5c36b
isaiah vang <vang.isaiah3@gmail.com>
2023-04-21 14:58:25 -0400

    finished constructors and getters/setters

--- src/main/java/com/iv/Main.java
+++ src/main/java/com/iv/Main.java
@@ -2,6 +2,66 @@ package com.iv;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Create Book class
+        // Add properties, constructors, and setters/getters
+        //
+    }
+}
+class Book{
+    private int id;
+    private String isbn;
+    private String title;
+    private boolean isCheckOut;
+    private String checkedOutTo;
+
+    // Constructors
+
+    public Book(int id, String isbn, String title, boolean isCheckOut, String checkedOutTo){
+        this.id = id;
+        this.isbn = isbn;
+        this.title = title;
+        this.isCheckOut = isCheckOut;
+        this.checkedOutTo = checkedOutTo;
+    }
+    // Getters/Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCheckOut() {
+        return isCheckOut;
+    }
+
+    public void setCheckOut(boolean checkOut) {
+        isCheckOut = checkOut;
+    }
+
+    public String getCheckedOutTo() {
+        return checkedOutTo;
+    }
+
+    public void setCheckedOutTo(String checkedOutTo) {
+        this.checkedOutTo = checkedOutTo;
     }
 }
